704989a7664e5c4968fbae808331d1a974876f86
rinelfi <elierijaniaina@gmail.com>
2021-11-18 07:18:52 +0300

    gestion de clique sur le contact de la fil de discussion

--- src/main/java/mg/rinelfi/app/ContactController.java
+++ src/main/java/mg/rinelfi/app/ContactController.java
@@ -1,31 +1,29 @@
 package mg.rinelfi.app;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import mg.rinelfi.Launcher;
 
 import java.io.IOException;
 
-public class ContactController extends Controller{
+public class ContactController extends Controller {
     @FXML
     private Label username, textMessage;
     
-    public void doOpenDiscussionAction() throws IOException {
-        FXMLLoader loader = new FXMLLoader(Launcher.class.getResource("/mg/rinelfi/app/TextDiscussionView.fxml"));
-        Parent view = loader.load();
-        ((Controller) loader.getController()).setStage(this.getStage());
-        Scene scene = new Scene(view);
-        this.getStage().setScene(scene);
-    }
-    
-    @Override
-    public void setStage(Stage stage) {
-        super.stage = stage;
-        super.stage.setTitle("Guest - chat app");
+    public void doOpenDiscussionAction(MouseEvent event) throws IOException {
+        if (MouseButton.SECONDARY == event.getButton()) {
+            System.out.println("popup");
+        } else if (MouseButton.PRIMARY == event.getButton()) {
+            FXMLLoader loader = new FXMLLoader(Launcher.class.getResource("/mg/rinelfi/app/TextDiscussionView.fxml"));
+            Parent view = loader.load();
+            ((Controller) loader.getController()).setStage(this.getStage());
+            Scene scene = new Scene(view);
+            this.getStage().setScene(scene);
+        }
     }
 }
